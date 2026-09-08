e92f1eb7019faaca886fb9cbd802a53a752f84a5
Shantanu Ravi <shaanravi133@gmail.com>
2026-09-08 17:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int ans;
+        if(n < 1000){
+            ans = 0;
+        }
+        else if(n >= 1000 && n <= 9999){
+            ans = n - 1000 + 1;
+        }
+        else if(n >= 10000 && n <= 99999){
+            ans = n - 1000 + 1;
+        }
+        return ans;   
+    }
+};
