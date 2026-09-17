e83faa751d298495c1032a443803d1effa8f1306
Shantanu Ravi <shaanravi133@gmail.com>
2026-09-17 23:56:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.19%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int bestEnding1 = nums[0];
-        int result1 = nums[0];
-        int bestEnding2 = nums[0];
-        int result2 = nums[0];
+        int bestMaxTilNow = nums[0];
+        int bestMinTilNow = nums[0];
         int result = nums[0];
-        for(int i = 1;i < nums.size();i++){
-            int prevPro1 = bestEnding1 * nums[i];
-            int startNew1 = nums[i];
-            int prevPro2 = bestEnding2 * nums[i];
-            int startNew2 = nums[i];
-            bestEnding1 = max({prevPro1, prevPro2, startNew1});
-            result1 = max(result1,bestEnding1);
-            bestEnding2 = min({prevPro1, prevPro2, startNew1});
-            result2 = min(result2,bestEnding2);
-            result = max(result1,result2);
+        for(int i = 1; i < nums.size(); i++){
+            int freshStart = nums[i];
+            int currPro1 = bestMaxTilNow * nums[i];
+            int currPro2 = bestMinTilNow * nums[i];
+            bestMaxTilNow = max(freshStart,max(currPro1,currPro2));
+            bestMinTilNow = min(freshStart,min(currPro1,currPro2));
+            result = max(result,max(bestMaxTilNow,bestMinTilNow));
         }
         return result;    
     }
